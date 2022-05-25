2e52511c96e82c29203f95bd5d6e0ac448af8586
annakonn200059 <konnova.anya2016@yandex.ru>
2022-05-25 23:48:46 +0400

    added users table for admin to change roles

--- react-resolution-super/src/components/ProfileTabsContent/Users/index.tsx
+++ react-resolution-super/src/components/ProfileTabsContent/Users/index.tsx
@@ -1,14 +1,51 @@
-import React, { FC, useEffect, useState } from 'react'
+import React, { FC, useCallback, useEffect, useState } from 'react'
 import * as ST from './styled'
 import { getAllUsers } from 'api/dashboard'
 import { User } from 'types/allUsers'
+import { Preloader } from '../../preloader'
+import { TableRow } from './tableRow'
 
 export const Users: FC = () => {
   const [users, setUsers] = useState<User[]>([])
+  const [isLoading, setLoading] = useState(false)
 
-  useEffect(() => {
-    getAllUsers().then((resp) => setUsers(resp.users))
+  const onGetAllUsers = useCallback(() => {
+    getAllUsers()
+      .then((resp) => setUsers(resp.users))
+      .finally(() => {
+        setLoading(false)
+      })
   }, [])
 
-  return <></>
+  useEffect(() => {
+    let isMounted = true
+    if (isMounted) {
+      setLoading(true)
+      onGetAllUsers()
+    }
+    return () => {
+      isMounted = false
+    }
+  }, [onGetAllUsers])
+
+  return (
+    <ST.Container>
+      {!isLoading ? (
+        <ST.Table>
+          <ST.Row header={true}>
+            <ST.Field>Name</ST.Field>
+            <ST.Field>Email</ST.Field>
+            <ST.Field>Date joined</ST.Field>
+            <ST.Field>Role</ST.Field>
+            <ST.Field>Edit role</ST.Field>
+          </ST.Row>
+          {users.map((user) => (
+            <TableRow key={user.id} userData={user} />
+          ))}
+        </ST.Table>
+      ) : (
+        <Preloader />
+      )}
+    </ST.Container>
+  )
 }
--- react-resolution-super/src/components/ProfileTabsContent/Users/styled.ts
+++ react-resolution-super/src/components/ProfileTabsContent/Users/styled.ts
@@ -1,1 +1,123 @@
 import styled from 'styled-components'
+import Edit from 'assets/icons/subscriptions.svg'
+import Cancel from 'assets/icons/cancel.svg'
+import { COLORS } from 'constants/colors'
+
+export const Container = styled.div`
+  padding: 50px 60px 0 60px;
+`
+
+export const Table = styled.div`
+  display: grid;
+  grid-template-rows: 1fr;
+  width: 100%;
+  border: 1px solid rgba(0, 0, 0, 0.125);
+  border-radius: 8px;
+`
+interface IField {
+  header?: boolean
+}
+
+export const Row = styled.div<IField>`
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr 1.2fr 0.4fr;
+  padding: 20px;
+  background-color: ${(props) => (props.header ? 'rgba(0,0,0,0.03)' : '')};
+  border-bottom: 1px solid rgba(0, 0, 0, 0.125);
+  font-weight: ${(props) => (props.header ? 'bold' : '')};
+  font-size: ${(props) => (props.header ? '18px' : '')};
+  align-items: center;
+
+  &:last-child {
+    border-bottom: none;
+  }
+`
+
+export const Field = styled.div<IField>`
+  margin: auto;
+`
+
+export const EditImage = styled.div`
+  width: 20px;
+  height: 20px;
+  margin: auto;
+  background: url(${Edit}) 50% 50% no-repeat;
+  cursor: pointer;
+  align-self: center;
+`
+
+export const CloseEdit = styled.div`
+  width: 20px;
+  height: 20px;
+  //margin: auto;
+  background: url(${Cancel}) 100% 100% no-repeat;
+  cursor: pointer;
+  align-self: center;
+`
+
+export const SubmitEdit = styled.button`
+  cursor: pointer;
+  background: none;
+  color: ${COLORS.blue};
+  font-size: 18px;
+  font-weight: bold;
+
+  &:disabled {
+    color: ${COLORS.lightGrey};
+    cursor: not-allowed;
+  }
+`
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin: auto;
+  column-gap: 10px;
+`
+
+export const RoundSwitch = styled.span``
+
+export const Label = styled.label`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  cursor: pointer;
+  font-weight: bold;
+`
+
+export const Switch = styled.div`
+  position: relative;
+  width: 60px;
+  height: 28px;
+  background: #b3b3b3;
+  border-radius: 32px;
+  padding: 4px;
+  transition: 300ms all;
+
+  &:before {
+    transition: 300ms all;
+    content: '';
+    position: absolute;
+    width: 28px;
+    height: 28px;
+    border-radius: 35px;
+    top: 50%;
+    left: 4px;
+    background: white;
+    transform: translate(0, -50%);
+  }
+`
+
+export const Input = styled.input`
+  opacity: 0;
+  position: absolute;
+
+  &:checked + ${Switch} {
+    background: ${COLORS.blue};
+
+    &:before {
+      transform: translate(32px, -50%);
+    }
+  }
+`
